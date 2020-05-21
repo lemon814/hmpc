3ab4ebca796a885cb5c43d1b59a0f70144a83a52
lemon814 <13821501560@163.com>
2020-05-21 16:25:43 +0800

    登陆组件创建和路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 导入登陆组件
+import Login from '../views/login/index.vue'
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/login', component: Login }
+]
 
 const router = new VueRouter({
   routes

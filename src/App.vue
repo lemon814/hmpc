853b1b8e20d2885457f1ce3a6dabbbb1f38a9bf1
lemon814 <13821501560@163.com>
2020-05-21 15:24:24 +0800

    引入element组件库，封装axios请求模板

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,66 @@
 <template>
   <div id="app">
     <h1>黑马头条内容发布系统</h1>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button plain>朴素按钮</el-button>
+      <el-button type="primary" plain>主要按钮</el-button>
+      <el-button type="success" plain>成功按钮</el-button>
+      <el-button type="info" plain>信息按钮</el-button>
+      <el-button type="warning" plain>警告按钮</el-button>
+      <el-button type="danger" plain>危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+
+    <el-button type="primary" @click="hTest">测试</el-button>
     <router-view/>
   </div>
 </template>
 
 <script>
+// 1. 导入封装好的模块 ajax这个名字是可以改换
+import ajax from './utils/request'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      value1: true
+    }
+  },
+  methods: {
+    hTest () {
+      console.log(1)
+      ajax({
+        method: 'GET',
+        URL: '/mp'
+      })
+    }
+  }
 }
 </script>>
 
---
+++ src/utils/request.js
@@ -0,0 +1,13 @@
+/**
+ * 基于 axios 封装的请求模块
+ */
+import axios from 'axios'
+
+// 创建一个 axios 实例，说白了就是复制了一个 axios
+// 我们通过这个实例去发请求，把需要的配置配置给这个实例来处理
+const ajax = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
+})
+
+// 导出请求方法
+export default ajax

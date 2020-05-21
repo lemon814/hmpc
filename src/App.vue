931856c096d20837b69b5a2f0e343cd1f884e849
lemon814 <13821501560@163.com>
2020-05-21 22:09:55 +0800

    核心功能实现

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,7 @@
 </template>
 
 <script>
-// 1. 导入封装好的模块 ajax这个名字是可以改换
 export default {
   name: 'App'
 }
 </script>>
-
-<style lang="less">
-
-</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,17 +12,20 @@
         <!-- 表单部分 -->
         <el-form class="login-form" ref="form">
             <el-form-item>
-            <el-input></el-input>
+              <!-- 手机号 -->
+              <el-input v-model.trim="user.mobile" placeholder="请输入手机号"></el-input>
             </el-form-item>
             <el-form-item>
-            <el-input></el-input>
+              <!-- 验证号 -->
+              <el-input v-model.trim="user.code" placeholder="请输入验证号"></el-input>
             </el-form-item>
 
             <el-form-item>
-            <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+              <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
             </el-form-item>
+
             <el-form-item>
-            <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+              <el-button class="login-btn" type="primary" @click="hLogin">登录</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -30,18 +33,55 @@
 </template>
 
 <script>
+// ajax名字是可以改的。它的格式与 axios()的格式是一样的
+import ajax from '../../utils/request.js'
 export default {
   name: 'Login',
-  components: {},
-  props: {},
   data () {
-    return {}
+    return {
+      // 设置数据项,双向绑定
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      checked: false
+    }
   },
-  computed: {},
-  watch: {},
-  created () {},
-  mounted () {},
-  methods: {}
+  methods: {
+    // 实现登陆功能
+    hLogin () {
+      // 1. 收集用户信息，简单判空
+      if (this.user.mobile === '') {
+        return
+      }
+
+      if (this.user.code === '') {
+        return
+      }
+
+      // 2. 检测是否同意
+      if (this.checked === false) {
+        return
+      }
+
+      // 3. 发根据接口文档的要求，发出ajax请求
+      //  1) 引入request.js
+      //  2) 发请求
+      ajax({
+        method: 'POST',
+        url: '/mp/v1_0/authorizations',
+        data: {
+          mobile: this.user.mobile,
+          code: this.user.code
+        }
+      }).then(res => {
+        alert('ok')
+        console.log(res.data)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 

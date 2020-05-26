266240d455182b11f7decd4b7d6304297676a347
lemon814 <13821501560@163.com>
2020-05-26 19:42:25 +0800

    通过请求拦截器来添加token

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,6 @@
  *
  * es6按需导入导出
  */
-import { getUser } from '../utils/storage'
 // ajax名字是可以改的。它的格式与 axios()的格式是一样的
 import ajax from '../utils/request'
 //  定义一个函数，并导出
@@ -27,16 +26,11 @@ export const userLogin = (mobile, code) => {
  * 这个接口是需要token信息才能访问的
  */
 export const userGetProfile = () => {
-  const userInfo = getUser()
   // 请求接口
   return ajax({
     method: 'GET',
-    url: '/mp/v1_0/user/profile',
-    // 参数写在headers中是由于 接口的要求
-    // 参数名，个数，类型，在哪里传过去......都是由后端写接口的人决定！
-    headers: {
-      // Authorization: 'Bearer 从localstorge中取出token字符串'
-      Authorization: `Bearer ${userInfo.token}`
-    }
+    url: '/mp/v1_0/user/profile'
+    // 设置改到了request.js中的请求拦截器
+    // 不再需要写token参数，会在请求拦截器自动添加
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,8 @@
 /**
  * 基于 axios 封装的请求模块
  */
+import { getUser } from './storage'
+
 import axios from 'axios'
 
 // 创建一个 axios 实例，说白了就是复制了一个 axios
@@ -9,6 +11,26 @@ const ajax = axios.create({
   // 基地址：当前项目所有的接口都是以这个地址开头的
   baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
   // baseURL: 'http://api-toutiao-web.itheima.net/'
+})
+
+// Add a request interceptor
+// 请求拦截器
+// 所有请求发出去之前会执行的函数，都会处理的逻辑。
+ajax.interceptors.request.use(function (config) {
+  const userInfo = getUser()
+  // Do something before request is sent
+  // 在请求发出去之前做一些事
+  console.log('ajax.interceptors.request', Date.now(), config)
+  // 本次请求采用的配置
+  // 补充一个headers参数Authorization
+  if (userInfo && userInfo.token) {
+    // 在这里给请求加上header，设置token
+    config.headers.Authorization = `Bearer ${userInfo.token}`
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
 })
 
 // 导出请求方法
